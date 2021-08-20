bedb9f18470e6aa23897bffb0d74d021f71611d3
E - Coders (Aasheesh Agarwal) <83082760+e-coders@users.noreply.github.com>
2021-08-20 15:12:11 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showPreview(){
-  var htmlCode = document.getElementById("htmlCode").value;
-  var cssCode = "<style>" + document.getElementById("cssCode").value +"</style>";
-  var jsCode = '<script type="text/javascript">' + document.getElementById("jsCode").value +"</script>";
-  var frame = document.getElementById("preview-window").contentWindow.document;
-  frame.open();
-  frame.write(htmlCode+cssCode+jsCode);
-  frame.close();
-}
